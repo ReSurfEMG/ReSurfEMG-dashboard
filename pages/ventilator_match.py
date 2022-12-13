33e6c4bd8b843b43a1a2d0cd140812d14c895f01
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2022-12-13 11:28:27 +0100

    slider buttons for inputs

--- pages/ventilator_match.py
+++ pages/ventilator_match.py
@@ -16,21 +16,72 @@ layout = dbc.Container(
         dcc.Store(id="store"),
         html.H1("Dynamically rendered tab content for ventilator and EMG (in progress)"),
         html.Hr(),
-        dbc.Button(
-            "Regenerate graphs",
-            color="primary",
-            id="button",
-            className="mb-3",
-        ),
+        
         dbc.Tabs(
             [
-                dbc.Tab(label="Original signal", tab_id="original_signal"),
                 dbc.Tab(label="Time corrected", tab_id="adjusted_signal"),
+                dbc.Tab(label="Original signal", tab_id="original_signal"),
+                
             ],
             id="tabs",
             active_tab="original_signal",
         ),
         html.Div(id="tab-content", className="p-4"),
+
+        dbc.Col([html.Div([
+                dcc.Input(
+                    id='time-shift-numb',
+                    type="number",
+                    placeholder="Time shift number",
+                    value=0,
+                    style={'color': 'green'},
+                ),
+            ],
+                style={'textAlign': 'right'}),
+            ], width=2,),
+        
+        dbc.Col([
+                html.Div('EMG time shift, milliseconds', style={'textAlign': 'left'})
+                ], width=2, style={"color": "green"}),
+
+            
+        dbc.Col([html.Div([
+                dcc.Input(
+                    id='emg-lead-numb',
+                    type="number",
+                    placeholder="EMG lead number",
+                    value=0,
+                    min=0,
+                    max= 25,
+                    style={'color': 'purple'}
+                ),
+            ],
+                style={'textAlign': 'right'}),
+            ], width=2),
+        
+        dbc.Col([
+                html.Div('EMG lead number', style={'textAlign': 'left'})
+                ], width=2, style={"color": "purple"}),
+
+        
+        dbc.Col([html.Div([
+                dcc.Input(
+                    id='vent-lead-numb',
+                    type="number",
+                    placeholder="Vent lead number",
+                    value=1,
+                    min=0,
+                    max=10,
+                ),
+            ],
+                style={'textAlign': 'right'}),
+            ], width=2),
+        
+        dbc.Col([
+                html.Div('Vent lead number', style={'textAlign': 'left'})
+                ], width=2),
+       
+       
     ]
 )
 

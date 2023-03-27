3e445cc9e219ab992cc44a18f190e29532396705
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2023-03-27 14:24:45 +0200

    copyright on files, and a workflow with linting

--- callbacks_emgventilator.py
+++ callbacks_emgventilator.py
@@ -1,3 +1,10 @@
+"""
+Copyright 2023 Netherlands eScience Center and University of Twente
+Licensed under the Apache License, version 2.0. See LICENSE for details.
+
+This file contains functions to work functions from the ReSurfEMG library.
+"""
+
 import utils
 from app import variables
 from dash import Input, Output, callback
--- pages/emg_ventilator.py
+++ pages/emg_ventilator.py
@@ -1,3 +1,10 @@
+"""
+Copyright 2023 Netherlands eScience Center and University of Twente
+Licensed under the Apache License, version 2.0. See LICENSE for details.
+
+This file contains functions to work functions from the ReSurfEMG library.
+"""
+
 import dash
 from dash import html, dcc
 import trace_updater

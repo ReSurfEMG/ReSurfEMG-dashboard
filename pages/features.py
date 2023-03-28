13867401813acee1c88c73947733ac275c42bf9e
ElineOppersma <e.oppersma@utwente.nl>
2023-03-28 10:30:01 +0200

    add variability calculation as breath detection method

--- pages/features.py
+++ pages/features.py
@@ -38,6 +38,7 @@ select_computation_card = dbc.Card([
             id=FEATURES_SELECT_COMPUTATION,
             options=[
                 {"label": "Entropy", "value": BreathSelectionMethod.ENTROPY},
+                {"label": "Variability", "value":BreathSelectionMethod.VARIABILITY},
             ],
             value=default_breath_method
         )
